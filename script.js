cb28857b2db48e24bbdef8c4ca6fedd2e3bdf72f
unknown <niconogales@alumnos.cei.es>
2023-12-19 16:29:38 +0100

    semaforo-clase

---
+++ script.js
@@ -0,0 +1,117 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     let rojo = document.getElementById("rojo")
+//     let amarillo = document.getElementById("amarillo")
+//     let verde = document.getElementById("verde")
+//     let boton = document.getElementById("darle-boton")
+//     let cambio = document.getElementById ("boton-cambio")
+//     let vuelta = document.getElementById ("boton-vuelta")
+
+
+    // rojo.addEventListener('click', function () {
+    //     //aqui va la logica con classlist.toggle para añadir una clase nueva a este elemento
+    //     rojo.classList.toggle('brillo')
+    //     amarillo.classList.remove('brillo')
+    //     verde.classList.remove('brillo')
+    // })
+    // amarillo.addEventListener('click', function () {
+    //     //aqui va la logica con classlist.toggle para añadir una clase nueva a este elemento
+    //     amarillo.classList.toggle('brillo')
+    //     rojo.classList.remove('brillo')
+    //     verde.classList.remove('brillo')
+    //     verde.addEventListener('click', function () {
+    //         //aqui va la logica con classlist.toggle para añadir una clase nueva a este elemento
+    //         verde.classList.toggle('brillo')
+    //         amarillo.classList.remove('brillo')
+    //         rojo.classList.remove('brillo')
+    //     })
+    // })
+
+
+    // setInterval(() => {
+    //             //aqui va la logica con classlist.toggle para añadir una clase nueva a este elemento
+    //     setTimeout(() => {
+    //             rojo.classList.toggle('brillo')
+    //             amarillo.classList.remove('brillo')
+    //             verde.classList.remove('brillo')
+    //     }, 0);        
+
+
+    //     setTimeout(() => {
+
+    //             //aqui va la logica con classlist.toggle para añadir una clase nueva a este elemento
+    //             amarillo.classList.toggle('brillo')
+    //             rojo.classList.remove('brillo')
+    //             verde.classList.remove('brillo')
+
+    //     }, 5000);
+
+    //     setTimeout(() => {
+
+    //             //aqui va la logica con classlist.toggle para añadir una clase nueva a este elemento
+    //             verde.classList.toggle('brillo')
+    //             amarillo.classList.remove('brillo')
+    //             rojo.classList.remove('brillo')
+
+    //     },10000);
+
+
+    // },15000)
+
+    // const intervalo = () => {
+    //     verde.classList.toggle('brillo')
+    //     amarillo.classList.remove('brillo')
+    //     rojo.classList.remove('brillo')
+
+    //     setInterval(() => {
+
+            //aqui va la logica con classlist.toggle para añadir una clase nueva a este elemento
+            // setTimeout(() => {
+            //     rojo.classList.toggle('brillo')
+            //     amarillo.classList.remove('brillo')
+            //     verde.classList.remove('brillo')
+            // }, 0);
+
+
+            // setTimeout(() => {
+
+            //     //aqui va la logica con classlist.toggle para añadir una clase nueva a este elemento
+            //     amarillo.classList.toggle('brillo')
+            //     rojo.classList.remove('brillo')
+            //     verde.classList.remove('brillo')
+
+            // }, 5000);
+
+            // setTimeout(() => {
+
+                //aqui va la logica con classlist.toggle para añadir una clase nueva a este elemento
+//                 verde.classList.toggle('brillo')
+//                 amarillo.classList.remove('brillo')
+//                 rojo.classList.remove('brillo')
+
+//             }, 10000);
+
+
+//         }, 15000)
+//     }
+
+//     boton.addEventListener('click', intervalo)
+
+//     cambio.addEventListener ("click",() => {
+//         rojo.style.backgroundColor = "pink"
+//         amarillo.style.backgroundColor = "blue"
+//         verde.style.backgroundColor = "brown"
+//         vuelta.addEventListener ('click',() =>{
+//             rojo.style.backgroundColor = "red"
+//             amarillo.style.backgroundColor = "yellow"
+//             verde.style.backgroundColor = "green"
+//         })
+//     })
+// })
+
+document.addEventListener("DOMContentLoaded", function(){
+
+let usuario = prompt ("Selecciona una opción entre piedra, papel o tijera")
+let pc = options [Math.floor(Math.random()*options.length)]
+})
+
+
